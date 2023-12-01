4f323853279a68bb9a3c75e61fb01c8a005cc46e
unknown <kieu24080@gmail.com>
2023-12-01 19:00:34 +0700

    bài tập

---
+++ btvnbuoi2.js
@@ -0,0 +1,8 @@
+let grapes = ["cam ngọt", "táo", "khế", "lê","chôm chôm","vải","nhãn","táo xanh","táo vàng","mít","xoài","trứng cá","ổi","tắc","chanh"]
+console.log("I have 15 grapes");
+console.log(grapes.length)
+for(let i=0;i<=grapes.length-1;i++){
+console.log(i + ": " + grapes[i])
+}
+
+
